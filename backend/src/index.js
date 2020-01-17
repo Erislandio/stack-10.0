c9c1171738b686a45ce662185e0e74cc0dcd3139
erislandio.soares <erislandiosoares21@gmail.com>
2020-01-16 21:30:49 -0300

    added backend config

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,27 +1,20 @@
 const express = require('express');
-const mongoose = require('mongoose')
 const routes = require('./routes')
+const mongoose = require("mongoose");
 
+const PORT = 3000
 const app = express()
-mongoose.connect('mongodb+srv://EduardoSoares:15975325@omnistack10-goels.gcp.mongodb.net/week10?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
 
 app.use(express.json())
 app.use(routes)
 
+const mongodbUri = "mongodb+srv://eris:Er1sl@ndio@cluster0-ecwcd.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(mongodbUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 
-// Método HTTP: GET (para receber informações), POST (criar informação), PUT(editar um recurdo da aplicação), DELETE (deletar)
-
-// Tipos de parâmetros
-
-// Query params: request.query (Filtros, ordenação, paginação, ...)
-// Route Params: request.params (Identificar um recurso na alteração ou remoção)
-// Body: request.body (Dados para criação ou alteração de um registro)
-
-//MongoDB (Não-relacional)
-
-
-app.listen(3333)
+app.listen(PORT, () => {
+    console.log('server online on http://localhost:3000')
+})
